6d1622385738b73af45f208ed64073eb2d8087b4
Saketram Durbha <durbhasaketram@gmail.com>
2018-02-05 09:37:00 -0700

    Use XboxController instead of Joystick class

--- src/main/kotlin/frc/team2036/robot/OperatorInput.kt
+++ src/main/kotlin/frc/team2036/robot/OperatorInput.kt
@@ -1,13 +1,13 @@
 package frc.team2036.robot
 
-import edu.wpi.first.wpilibj.Joystick
+import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.buttons.JoystickButton
 import frc.team2036.robot.ramp.toggleRamp
 import frc.team2036.robot.util.LogType
 import frc.team2036.robot.util.logger
 
 //Stores a global joystick object
-val joystick = Joystick(config("ports")["joystick"] as Int)
+val joystick = XboxController(config("ports")["joystick"] as Int)
 
 //Binds joystick buttons to commands using ports defined in Config; called from Robot
 fun initButtons() {
