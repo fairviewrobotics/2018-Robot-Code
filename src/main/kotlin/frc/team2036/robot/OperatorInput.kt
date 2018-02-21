5c854e0c4b9123e89b4c067077e7f6f43dd5a7fe
Kyle Pfromer <kpfromer2@gmail.com>
2018-02-20 17:36:33 -0700

    Move elevator buttons and rumble to initButton

--- src/main/kotlin/frc/team2036/robot/OperatorInput.kt
+++ src/main/kotlin/frc/team2036/robot/OperatorInput.kt
@@ -1,14 +1,32 @@
 package frc.team2036.robot
 
+import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj.buttons.JoystickButton
+import frc.team2036.robot.elevator.toBottom
+import frc.team2036.robot.elevator.toTop
 import frc.team2036.robot.util.LogType
 import frc.team2036.robot.util.logger
 
-//Stores a global joystick object
-val joystick = XboxController(config("ports")["joystick"] as Int)
-val joystick2 = XboxController(config("ports")["joystick2"] as Int)
+//Stores a global koolKoyJoystick object
+val koolKoyJoystick = XboxController(config("ports")["koolKoyJoystick"] as Int)
+val koolKirljoystick = XboxController(config("ports")["koolKirljoystick"] as Int)
 
-//Binds joystick buttons to commands using ports defined in Config; called from Robot
+//Binds koolKoyJoystick buttons to commands using ports defined in Config; called from Robot
 fun initButtons() {
     logger.log("Joystick", "Binding commands to buttons.", LogType.TRACE)
+
+    val elevatorToTopButton = JoystickButton(koolKirljoystick, 4)
+    val elevatorToBottomButton = JoystickButton(koolKirljoystick, 1)
+
+    elevatorToTopButton.whenPressed(toTop)
+    elevatorToBottomButton.whenPressed(toBottom)
+}
+
+/**
+ * Sets rumble for controller 2 for both hands
+ */
+fun rumble(rumble: Double) {
+    koolKirljoystick.setRumble(GenericHID.RumbleType.kLeftRumble, rumble)
+    koolKirljoystick.setRumble(GenericHID.RumbleType.kRightRumble, rumble)
 }
